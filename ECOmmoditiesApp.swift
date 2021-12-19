1257cd1cc749b58a9b67c5cfe20edea0f6e60aa1
shannonjliu <69657484+shannonjliu@users.noreply.github.com>
2021-12-19 11:16:27 -0500

    Create ECOmmoditiesApp.swift

---
+++ ECOmmoditiesApp.swift
@@ -0,0 +1,18 @@
+//
+//  ECOmmoditiesApp.swift
+//  ECOmmodities
+//
+//  Created by Shannon Liu on 12/18/21.
+//
+
+import SwiftUI
+
+@main
+struct ECOmmoditiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+            
+        }
+    }
+}
